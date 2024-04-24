09d0123d6e2e99c364a721e1220bb5923d73ba03
LinaMahrouch <lina.mahrouch01@gmail.com>
2024-04-25 00:43:27 +0100

    Fix JSON overflow

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -11,18 +11,18 @@ export default function Index() {
 
   return (
     <div className=" text-white">
-      <div className="py-20 text-center mx-auto">
-        <div className="flex items-center justify-center mb-2">
-          <h1 className="text-4xl font-bold"> Welcome to Remix </h1>
-          <img src="/images/cd.png" alt="Remix Logo" className="h-12 ml-2" />
-          <h1 className="text-4xl font-bold"> + Hugging Face</h1>
-          <img src="/images/hf-logo.png" alt="Hugging Face Logo" className="h-12 ml-2" />
+      <div className="py-10 md:py-20 text-center mx-auto px-4">
+        <div className="flex flex-col md:flex-row items-center justify-center mb-2">
+          <h1 className="text-2xl md:text-4xl font-bold"> Welcome to Remix </h1>
+          <img src="/images/cd.png" alt="Remix Logo" className="h-8 md:h-12 mx-2 my-2" />
+          <h1 className="text-2xl md:text-4xl font-bold">+ Hugging Face</h1>
+          <img src="/images/hf-logo.png" alt="Hugging Face Logo" className="h-8 md:h-12 mx-2 my-2" />
         </div>
-        <h2 className="text-3xl font-bold">Transformers Template</h2>
+        <h2 className="text-xl md:text-3xl font-bold">Transformers Template</h2>
       </div>
 
       <div className="container mx-auto mt-2 p-4">
-        <div className="max-w-md mx-auto bg-zinc-800 p-6 rounded-lg shadow-md">
+        <div className="max-w-lg mx-auto bg-zinc-800 p-6 rounded-lg shadow-md">
           <input
             type="text"
             className="w-full border-gray-600 focus:border-indigo-800 focus:ring focus:ring-indigo-800 rounded-md px-4 py-2 mb-4 bg-zinc-900 text-gray-300"
@@ -38,7 +38,7 @@ export default function Index() {
           </button>
           <div className="mt-4">
             {fetcher.data ? (
-              <pre className="bg-gray-900 p-4 rounded">{JSON.stringify(fetcher.data, null, 2)}</pre>
+              <pre className="bg-gray-900 p-4 rounded overflow-x-auto whitespace-pre-wrap">{JSON.stringify(fetcher.data, null, 2)}</pre>
             ) : (
               <p className="text-gray-400">Enter text to analyze</p>
             )}
