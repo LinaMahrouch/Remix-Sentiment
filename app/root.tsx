cd4f5bafb8a2d31b2995f3e9124e0e77afabe4f3
LinaMahrouch <lina.mahrouch01@gmail.com>
2024-04-24 06:03:13 +0100

    Added Vercel Web Analytics

--- app/root.tsx
+++ app/root.tsx
@@ -5,7 +5,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from "@remix-run/react";
-
+import { Analytics } from "@vercel/analytics/react"
 import stylesheet from "./tailwind.css?url";
 import { LinksFunction } from "@remix-run/node";
 
@@ -26,6 +26,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         {children}
         <ScrollRestoration />
         <Scripts />
+        <Analytics />
       </body>
     </html>
   );
